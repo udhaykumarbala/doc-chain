f22ae7504516457267d48538ec9921dbd8f3477a
udhaykumarbala <udhayk97@gmail.com>
2024-03-08 21:06:54 +0530

    port change

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get('*', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
